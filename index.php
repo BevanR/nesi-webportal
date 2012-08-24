f7b34c523b150f931a8723106216bb5e57eb47fc
Daniel Eberhardt <daniel@incontrast.co.nz>
2012-08-24 17:12:58 +1200

    Home hero banners styled to a beta stage

--- index.php
+++ index.php
@@ -44,25 +44,32 @@
 	<div class="hero hero-full">
 		<div class="carousel slide" id="home-hero">
 		<div class="carousel-inner">
-			<div class="item active">
-				<img src="/assets/img/Diwali_banner_1_2.jpeg">
-				<div class="carousel-caption">
-					<h4>First Thumbnail label</h4>
-					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+			<div class="item active" id="slide-research-application">
+				<div class="container">
+					<img src="/assets/img/banners/nesi-logo-big.png" />
+					<div class="slide-content">
+						<h2 class="slide-title">Research applications <br/>now open</h2>
+						<p><a href="#" class="slide-link">Apply Within</a></p>
+					</div>
 				</div>
 			</div>
-			<div class="item">
-				<img src="/assets/img/Diwali_banner_2_2.jpeg">
-				<div class="carousel-caption">
-					<h4>First Thumbnail label</h4>
-					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+			<div class="item" id="slide-national-roadshow">
+
+				<div class="container">
+					<img src="/assets/img/banners/roadshow-map.png" />
+					<div class="slide-content">
+						<h2 class="slide-title">National Roadshow <br/>September 2012</h2>
+						<p><a href="#" class="slide-link">Learn more</a></p>
+					</div>
 				</div>
 			</div>
-			<div class="item">
-				<img src="/assets/img/Diwali_banner_3_2.jpeg">
-				<div class="carousel-caption">
-					<h4>First Thumbnail label</h4>
-					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+			<div class="item" id="slide-case-study">
+				<div class="container">
+				<img src="/assets/img/banners/case-study.jpg" />
+					<div class="slide-content">
+						<h2 class="slide-title">Modelling glaciers <br/>of the southern alps</h2>
+						<p><a href="#" class="slide-link">Read the full case study</a></p>
+					</div>
 				</div>
 			</div>
 		</div>
