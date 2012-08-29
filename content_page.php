ef499b706835223d9da1847f781739a0528cc6cc
Daniel Eberhardt <daniel@incontrast.co.nz>
2012-08-29 14:39:57 +1200

    Base content page styled
    Still need to do some work in this area but that will have to wait until we get some proper content in there.

---
+++ content_page.php
@@ -0,0 +1,56 @@
+<?php include('_inc/_html_head.php') ?>
+
+<body class="single-post">
+	<?php include('_inc/_navbar.php') ?>
+
+	<div class="container">
+		<div class="row-fluid">
+			<div class="breadcrumbs">
+				<ol>
+					<li><a href="#">Access and Allocations</a></li>
+					<li><a href="#">Access</a></li>
+				</ol>
+			</div>
+			<div class="span8">
+				<article role="main" class="post">
+					<header>
+						<h1>Access Policy</h1>
+					</header>
+
+					<div class="entry">
+
+						<p>Our Access Policy defines the terms and conditions and mechanisms for providing allocations and access on our High Performance Computing facilities.</p>
+
+						<p>To advise us on the formation and reivew of this policy, we have an Access Policy Advisory Committee whose members represent computational science expertise alongside knowledge of the New Zealand research systems funding and management mechanisms.</p>
+
+						<p>The Access Policy is currently being drafted, and once approved will be available here for review.</p>
+					</div>
+				</article>
+			</div>
+			<div class="span2 offset1">
+				<nav class="sub-nav">
+					<h2>Access and Allocations</h2>
+					<ul>
+						<li><a href="#">Apply</a></li>
+						<li><a href="#">Acceptable Use Policy</a></li>
+						<li><a href="#">Access Policy</a></li>
+						<li><a href="#">Acknowledgement and Publication</a></li>
+						<li><a href="#">Allocations, Allocation Classes, Review Cycle</a></li>
+						<li><a href="#">Calls Timetable</a></li>
+						<li><a href="#">Eligibility</a></li>
+						<li><a href="#">Files and Data</a></li>
+						<li><a href="#">Projects</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div> <!-- /Row Fluid -->
+	</div> <!-- /container -->
+
+	<?php include('_inc/_footer.php') ?>
+	<?php include('_inc/_footer_scripts.php') ?>
+
+	<script type="text/javascript">
+
+	</script>
+	</body>
+</html>
