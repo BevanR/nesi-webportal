e677000ca7006047fb18fa4750318021dce9ea5f
Nathaniel Dobbin <ndobbin@gmail.com>
2012-09-17 14:35:44 +1200

    Page footer - needs further work - currently static.

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -39,5 +39,90 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+
+<div class="container">
+		<div class="row-fluid directorynav">
+			<nav class="span3">
+				<h3>Services</h3>
+				<ul class="unstyled">
+					<li><a href="#">HPC Online</a></li>
+					<li><a href="#">Research data</a></li>
+					<li><a href="#">Data Fabric</a></li>
+					<li><a href="#">Data Transfer</a></li>
+				</ul>
+			</nav>
+			<nav class="span3">
+				<h3>Access & Allocations</h3>
+				<ul class="unstyled">
+					<li><a href="#">Calculator</a></li>
+					<li><a href="#">Propose</a></li>
+					<li><a href="#">Access Policy</a></li>
+				</ul>
+			</nav>
+			<nav class="span3">
+				<h3>Support</h3>
+				<ul class="unstyled">
+					<li><a href="#">Submit a Request</a></li>
+					<li><a href="#">Knowledge base</a></li>
+					<li><a href="#">FAQ</a></li>
+				</ul>
+			</nav>
+			<nav class="span3">
+				<h3>About NeSI</h3>
+				<ul class="unstyled">
+					<li><a href="#">News</a></li>
+					<li><a href="#">Events</a></li>
+					<li><a href="#">Facilitites</a></li>
+					<li><a href="#">Case Studies</a></li>
+				</ul>
+			</nav>
+		</div>
+		</div>
+	</div> <!-- /container -->
+
+	<div class="container">
+		<div class="row-fluid collaborators">
+			<div>
+				<h3 class="span3">Our collaborators</h3>
+				<div class="span9">
+					<ul class="unstyled inline-list">
+						<li>
+							<a href="#">
+								<img src="assets/img/logo-u-of-a.jpg" alt="logo">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="assets/img/logo-u-of-c.jpg" alt="logo">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="assets/img/logo-niwa.jpg" alt="logo">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="assets/img/logo-ministry-of-si.jpg" alt="logo">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="assets/img/logo-manaaki-whenua.jpg" alt="logo">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="assets/img/logo-u-of-o.jpg" alt="logo">
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer>
+			<p class="legal">&copy;2011 New Zealand eScience Infrastructure. All rights reserved</p>
+	</footer>
 </body>
 </html>
