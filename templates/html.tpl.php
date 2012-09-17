ee31c32c896b86f3bd74a1fc6ac0137347606fc7
Nathaniel Dobbin <ndobbin@gmail.com>
2012-09-17 16:23:32 +1200

    Fix css.

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -15,12 +15,12 @@
   <?php print $scripts; ?>
 
 	<!-- Le styles -->
+	<link href="<?php print $directory; ?>/assets/css/screen.css" rel="stylesheet">
 	<style>
 		body {
 		padding-top: 90px; /* 90px to make the container go all the way to the bottom of the topbar */
 		}
 	</style>
-	<link href="<?php print $directory; ?>/assets/css/responsive.css" rel="stylesheet">
 
   <!-- HTML5 element support for IE6-8 -->
   <!--[if lt IE 9]>
