d571928a0ace9f5fddb2c97585fcaf89ec170892
Nathaniel Dobbin <ndobbin@gmail.com>
2012-10-16 05:55:17 +1300

    Add static template for frontpage until all views are migrated.

---
+++ themes/twitter_bootstrap/nesi_bootstrap/templates/page--front.tpl.php
@@ -0,0 +1,214 @@
+	<div class="navbar navbar-fixed-top">
+	<div class="navbar-inner">
+	<div class="container">
+		<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		</a>
+		<a class="brand" href="/"><span>NeSI &ndash; New Zealand eScience Infrastructure</span></a>
+		<div class="nav-collapse collapse">
+		<nav id="pnav">
+		<ul class="nav">
+			<li><a href="/?q=access-policy">Access</a></li>
+			<li><a href="/?q=facilities">HPC Facilities</a></li>
+			<li><a href="/?q=case-studies">Projects &amp; Case studies</a></li>
+			<li><a href="/?q=background">About Nesi</a></li>
+		</ul>
+		</nav>
+		<nav id="global-nav">
+		<ul class="nav pull-right">
+			<li><a href="#">Support</a></li>
+			<li><a href="/rebuild/user">Login</a></li>
+			<!--<li class="dropdown">
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Account <b class="caret"></b></a>
+			<ul class="dropdown-menu">
+				<li class="current-user">
+					<a href="/username" class="account-summary account-summary-small js-nav" data-nav="profile">
+					      <div class="content">
+					        <div class="account-group">
+					          <img class="avatar size32" src="https://twimg0-a.akamaihd.net/profile_images/2255495760/lookylooky_normal.jpg" alt="Daniel Eberhardt">
+					          <b class="fullname">Daniel Eberhardt</b>
+					          <small class="metadata">
+					              View my profile page
+					          </small>
+					        </div>
+					      </div>
+					    </a>
+				</li>
+				<li class="divider"></li>
+				<li><a href="#">Email</a></li>
+				<li><a href="#">Another action</a></li>
+				<li><a href="#">Something else here</a></li>
+				<li class="divider"></li>
+				<li><a href="#">Settings</a></li>
+				<li><a href="#">Sign out</a></li>
+			</ul>
+			</li>-->
+		</ul>
+		</nav>
+		</div><!--/.nav-collapse -->
+	</div>
+	</div>
+</div>
+	<div class="hero hero-full">
+	<div class="carousel slide" id="home-hero">
+	<div class="carousel-inner">
+		<div class="item active" id="slide-research-application">
+			<div class="container">
+				<img src="<?php print $directory; ?>/assets/img/banners/nesi-logo-big.png" />
+				<div class="slide-content">
+					<h2 class="slide-title">Research applications <br/>now open</h2>
+					<p><a href="#" class="slide-link">Apply Within</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item" id="slide-national-roadshow">
+
+			<div class="container">
+				<img src="<?php print $directory; ?>/assets/img/banners/roadshow-map.png" />
+				<div class="slide-content">
+					<h2 class="slide-title">National Roadshow <br/>September 2012</h2>
+					<p><a href="#" class="slide-link">Learn more</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item" id="slide-case-study">
+			<div class="container">
+			<img src="<?php print $directory; ?>/assets/img/banners/case-study.jpg" />
+				<div class="slide-content">
+					<h2 class="slide-title">Modelling glaciers <br/>of the southern alps</h2>
+					<p><a href="#" class="slide-link">Read the full case study</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<a class="carousel-control left" href="#home-hero" data-slide="prev">&lsaquo;</a>
+	<a class="carousel-control right" href="#home-hero" data-slide="next">&rsaquo;</a>
+	</div>
+</div>
+
+	<div class="container page-intro">
+		<section class="home-intro">
+			<h1>About NeSI</h1>
+			<div class="row-fluid">
+			<div class="span4">
+				<article class="sub-feature" id="bestgrid-intro">
+					<h1 class="feature-title">BeSTGRID Data Fabric</h1>
+					<p>Broadband enabled Science and Technology grid. <a href="#" class="l-more">Learn more</a></p>
+				</article>
+			</div>
+			<div class="span4">
+				<article class="sub-feature" id="facilities-intro">
+					<h1 class="feature-title"><abbr class="tip" rel="tooltip" data-animation="true" data-placement="top" title="High Performance Computing" title="High Performance Computing">HPC</abbr> Facilities</h1>
+					<p>Learn all about the facilities that NeSI collaborates with. <a href="/?q=facilities" class="l-more">Learn more</a></p>
+				</article>
+			</div>
+			<div class="span4">
+				<article class="sub-feature" id="calculator-intro">
+					<h1 class="feature-title"><abbr class="tip" rel="tooltip" data-animation="true" data-placement="top" title="High Performance Computing" title="High Performance Computing">HPC</abbr> Calculator</h1>
+					<p>Calculate the resourses and allocations requird for your next project. <a href="/rebuild/hpc-calc" class="l-more">Learn more</a></p>
+				</article>
+			</div>
+			</div>
+			<div class="row-fluid">
+				<p class="page-summary">NeSI enables our scientists across a wide range of communities and disciplines to access and utilise vastly superior computing power, achieving this through better more efficient coordination and cooperation across the research sector.</p>
+			</div>
+		</section>
+	</div> <!-- /container -->
+
+	<div class="container news-events-overview">
+		<div class="row-fluid">
+
+			<div class="span6 news-overview">
+				<section>
+					<h1>Latest News</h1>
+
+					<article class="o-item news-item">
+						<header>
+							<h1 class="o-title"><a href="/news_article.php">NZ researchers apply for 700,000 CPU core hours for amazing science</a></h1>
+							<p class="meta meta-date">Jun 7 <span class="yr">2012</span></p>
+						</header>
+							<div class="summary">
+								<p>The New Zealand eScience Infrastructure (NeSI) received applications for about 700,000 CPU core hours in its first call for access proposals from New Zealand researchers.</p>
+							</div>
+					</article>
+
+					<article class="o-item news-item">
+						<header>
+							<h1 class="o-title"><a href="/news_article.php">Vacancy: Education leader now open.</a></h1>
+							<p class="meta meta-date">Jun 7 <span class="yr">2012</span></p>
+						</header>
+							<div class="summary">
+								<p>The Education Programme Leader leads activities for NeSI's professional education programme. This programme supports research communities across New Zealand with their ability to utilise High Performance Computing. </p>
+							</div>
+					</article>
+
+					<article class="o-item news-item">
+						<header>
+							<h1 class="o-title"><a href="/news_article.php">Podcast: "Does this science compute?"</a></h1>
+							<p class="meta meta-date">Jun 7 <span class="yr">2012</span></p>
+						</header>
+							<div class="summary">
+								<p>Peter Griffin from Sciblogs recently interviewed our Director, Nick Jones as part of their weekly podcast. It is now available for download. The pair discussed the current call for access proposals as well as a few applications of NeSI's HPC facilities.</p>
+							</div>
+					</article>
+
+					<footer>
+						<a href="">More News</a>
+					</footer>
+				</section>
+			</div>
+
+			<div class="span6 events-overview">
+				<section>
+					<h1>Latest Events</h1>
+
+					<article class="o-item event-item event-type-roadshow">
+						<header>
+							<h1 class="o-title"><a href="#">National Roadshow - September 2012</a></h1>
+						</header>
+						<div class="summary">
+							<p>The team will visiting Dunedin, Christchurch, Wellington, Hamilton and Auckland, spending a day at each city.</p>
+						</div>
+						<aside class="event-dates">
+							<p class="meta meta-date">Event Dates <span class="event-date-from">10/09/12</span> &ndash; <span class="event-date-to">14/09/12</span></p>
+						</aside>
+					</article>
+
+					<article class="o-item event-item event-type-seminar">
+						<header>
+							<h1 class="o-title"><a href="#">"Modelling the superheating of small gallium clusters" - Christchurch</a></h1>
+						</header>
+						<aside class="event-dates">
+							<p class="meta meta-date">Seminar <span class="event-date-from">20/08/12</span></p>
+						</aside>
+					</article>
+
+					<article class="o-item event-item event-type-seminar">
+						<header>
+							<h1 class="o-title"><a href="#">"Discussing the What, Why and How To of NeSI" - Auckland</a></h1>
+						</header>
+						<aside class="event-dates">
+							<p class="meta meta-date">Seminar <span class="event-date-from">20/08/12</span></p>
+						</aside>
+					</article>
+
+					<article class="o-item event-item event-type-seminar">
+						<header>
+							<h1 class="o-title"><a href="#">"Benefiting from the New Zealand eScience Infrastructure" Wellington</a></h1>
+						</header>
+						<aside class="event-dates">
+							<p class="meta meta-date">Seminar <span class="event-date-from">20/08/12</span></p>
+						</aside>
+					</article>
+					<footer>
+						<a href="">More Events</a>
+					</footer>
+				</section>
+			</div>
+		</div> <!-- /Row Fluid -->
+	</div> <!-- /container -->
+
+	<script src="<?php print $directory; ?>/assets/js/jquery.lettering.js"></script>
+	<script src="<?php print $directory; ?>/assets/js/custom.js"></script>
