02505645fb39e7abdbc862f624702b6d076dcf74
Nathaniel Dobbin <ndobbin@gmail.com>
2012-10-19 05:12:02 +0000

    Update page to use dynamic directory footer.

--- themes/twitter_bootstrap/nesi_bootstrap/templates/page--front.tpl.php
+++ themes/twitter_bootstrap/nesi_bootstrap/templates/page--front.tpl.php
@@ -95,5 +95,9 @@
 		</div> <!-- /Row Fluid -->
 	</div> <!-- /container -->
 
+  <?php print render($page['directory']); ?>
+
+  <?php print render($page['collaborators']); ?>
+
 	<script src="<?php print $directory; ?>/assets/js/jquery.lettering.js"></script>
 	<script src="<?php print $directory; ?>/assets/js/custom.js"></script>
