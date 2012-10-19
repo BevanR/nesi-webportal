9dec8b61d01fe2fb0e69fa3544197f538f630820
Nathaniel Dobbin <ndobbin@gmail.com>
2012-10-19 04:21:39 +0000

    Added header template.

--- themes/twitter_bootstrap/nesi_bootstrap/templates/page.tpl.php
+++ themes/twitter_bootstrap/nesi_bootstrap/templates/page.tpl.php
@@ -1,56 +1,6 @@
-<header id="navbar" role="banner" class="navbar navbar-fixed-top">
-  <div class="navbar-inner">
-  	<div class="container">
-  	  <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-  	  <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-  		<span class="icon-bar"></span>
-  		<span class="icon-bar"></span>
-  		<span class="icon-bar"></span>
-  	  </a>
-  	  
-		<a class="brand" href="/"><span>NeSI &ndash; New Zealand eScience Infrastructure</span></a>
-  	  <div class="nav-collapse">
-    	  <nav id="pnav" role="navigation">
-		<ul class="nav">
-			<li><a href="/?q=access-policy">Access</a></li>
-			<li><a href="/?q=facilities">HPC Facilities</a></li>
-			<li><a href="/?q=case-studies">Projects &amp; Case studies</a></li>
-			<li><a href="/?q=background">About Nesi</a></li>
-		</ul>
-      		<?php if ($primary_nav): ?>
-      		  <?php //print $primary_nav; ?>
-      		<?php endif; ?>
-      	  
-      		<?php if ($search): ?>
-      		  <?php if ($search): print render($search); endif; ?>
-      		<?php endif; ?>
-      		
-      		<?php if ($secondary_nav) { ?>
-      		  <?php print $secondary_nav; ?>
-      		<?php } else { ?>
-      <nav id="global-nav">
-      <ul class="nav pull-right">
-        <li><a href="https://support.nesi.org.nz/">Support</a></li>
-        <li><a href="/portal/user/login">Login</a></li>
- 	    </ul>
-		  </nav>
 
-          <?php }; ?>
-    		</nav>
-  	  </div>         
-  	</div>
-  </div>
-</header>
 
-<div class="container">
-
-  <header role="banner" id="page-header">
-    <?php if ( $site_slogan ): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-  </header> <!-- /#header -->
+  <?php print render($page['header']); ?>
 	
 	<div class="row">
 	  
