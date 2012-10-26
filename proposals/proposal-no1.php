047660d1b514b1dc133fbe8f666d6eed77d97b37
Daniel Eberhardt <daniel@incontrast.co.nz>
2012-10-27 11:12:04 +1300

    Added new proposal include for ease of updating

--- proposals/proposal-no1.php
+++ proposals/proposal-no1.php
@@ -16,33 +16,7 @@
 		<div class="row-fluid">
 			<div class="span9">
 				<section>
-					<header>
-						<h1 class="page-header">Current Proposal</h1>
-					</header>
-
-					<ul class="nav nav-tabs">
-						<li class="active">
-							<a href="/proposals/proposal-no1.php" class="active">View</a>
-						</li>
-						<li class="">
-							<a href="/proposals/proposal-no1-edit.php">Edit</a>
-						</li>
-						<li class="">
-							<a href="/proposals/proposal-no1-team.php">Manage Team</a>
-						</li>
-					</ul>
-					<article role="main" class="proposal">
-						<header>
-							<h1>Regional Climate Simulations of New Zealand recent past climate change episodes</h1>
-							<div class="meta">
-								<p class="date">Submitted by <em>user name</em> on 31 May 2012 10:21</p>
-								<p>Proposal Type: Research</p>
-								<div id="proposal-status">
-									<p><em>Current Status:</em> <strong>In draft</strong></p>
-									<input type="submit" value="Submit proposal" class="btn btn-primary" id="proposal-submission-btn"/>
-								</div>
-							</div>
-						</header>
+					<?php $active = 1; include('../_inc/_proposal-no1-header.php') ?>
 
 
 						<aside class="proposal-allocation-requests">
