997c9e60074218601fb8075fe2476da0899d33e0
Nathaniel Dobbin <ndobbin@gmail.com>
2012-11-07 17:00:25 +1300

    Contact page template.

---
+++ themes/twitter_bootstrap/nesi_bootstrap/templates/page--contact.tpl.php
@@ -0,0 +1,55 @@
+  <?php print render($page['header']); ?>
+  <?php print render($page['subheader']); ?>
+	
+<div class="container contact">
+	  
+  <?php if ($breadcrumb): ?>
+	<div class="row-fluid">
+    <div class="breadcrumbs"><?php print $breadcrumb; ?></div>
+  </div>
+  <?php endif;?>
+  <div class="row-fluid">
+    <?php if ($page['sidebar_first']): ?>
+      <aside class="span3" role="complementary">
+        <?php print render($page['sidebar_first']); ?>
+      </aside>  <!-- /#sidebar-first -->
+    <?php endif; ?>  
+	  
+	  <section class="<?php //print _twitter_bootstrap_content_span($columns); ?>span8">  
+      <?php if ($page['highlighted']): ?>
+        <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
+      <?php endif; ?>
+      <!--<a id="main-content"></a>-->
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="page-header"><?php print $title; ?></h1> 
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php if ($tabs): ?>
+        <?php print render($tabs); ?>
+      <?php endif; ?>
+      <?php if ($page['help']): ?> 
+        <div class="help"><?php print render($page['help']); ?></div>
+      <?php endif; ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+	  </section>
+
+    <?php if ($page['sidebar_second']): ?>
+      <aside class="span3" role="complementary">
+        <?php print render($page['sidebar_second']); ?>
+      </aside>  <!-- /#sidebar-second -->
+    <?php endif; ?>
+
+  </div>
+</div>
+
+  <?php print render($page['directory']); ?>
+
+  <?php print render($page['collaborators']); ?>
+
+  <?php print render($page['footer']); ?>
+
