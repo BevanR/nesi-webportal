bc2bb4e9867eb2ab8b8a810e6e664ff429447970
Nathaniel Dobbin <ndobbin@gmail.com>
2012-11-09 17:31:52 +1300

    Update to case-study template.

--- themes/twitter_bootstrap/nesi_bootstrap/templates/node--casestudy.tpl.php
+++ themes/twitter_bootstrap/nesi_bootstrap/templates/node--casestudy.tpl.php
@@ -15,9 +15,13 @@
       </p>
     <?php endif; ?>
   </header>
+  <div class="main-cs-image">
   
+    <figure><?php print render($content['field_image']); ?></figure>
+  </div>
   <div class="entry">
 
+   <blockquote><p><?php print $content['field_pull_out_text'][0]['#markup']; ?></p></blockquote>
       <?php print render($content['field_tags']); ?>
   <?php
     // Hide comments, tags, and links now so that we can render them later.
@@ -25,9 +29,8 @@
     hide($content['links']);
     hide($content['field_tags']);
     hide($content['field_pull_out_text']);
-    //kpr($content);
-    
-    print render($content);
+    print $content['body'][0]['#markup'];
+    //print render($content);
   ?>
   </div>
 
