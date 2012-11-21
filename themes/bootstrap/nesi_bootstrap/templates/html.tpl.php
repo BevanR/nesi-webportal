8ce455344707d5e67161d5f226c2e6ccd9611ed8
Nathaniel Dobbin <ndobbin@gmail.com>
2012-11-21 13:05:58 +1300

    Remove duplicate reference to favicon.

--- themes/bootstrap/nesi_bootstrap/templates/html.tpl.php
+++ themes/bootstrap/nesi_bootstrap/templates/html.tpl.php
@@ -22,7 +22,6 @@
   <![endif]-->
 
 	<!-- Le fav and touch icons -->
-	<link rel="shortcut icon" href="/<?php print $directory; ?>/assets/ico/favicon.ico">
 	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/<?php print $directory; ?>/assets/ico/apple-touch-icon-144-precomposed.png">
 	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/<?php print $directory; ?>/assets/ico/apple-touch-icon-114-precomposed.png">
 	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/<?php print $directory; ?>/assets/ico/apple-touch-icon-72-precomposed.png">
