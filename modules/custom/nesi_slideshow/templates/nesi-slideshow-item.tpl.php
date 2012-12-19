67e5f62da552d5a41b666de8477fb94a9f8f3869
Nathaniel Dobbin <ndobbin@gmail.com>
2012-12-19 20:00:52 +0000

    Slide template files.

---
+++ modules/custom/nesi_slideshow/templates/nesi-slideshow-item.tpl.php
@@ -0,0 +1,12 @@
+<div class="item<?php print $active; ?>" style="background-color:#<?php print $banner_bg_color; ?>;">
+  <div class="container">
+    <img src="<?php print $image_path; ?>">
+    <div class="slide-content">
+      <h2 class="slide-title">
+        <span class="line1" style="color:#FFFFFF; background-color:#<?php print $primary_bg_color; ?>;"><?php print $primary_title; ?></span>
+        <span class="line2" style="color:#FFFFFF; background-color:#<?php print $secondary_bg_color; ?>;"><?php print $secondary_title; ?></span>
+      </h2>
+      <p><a class="slide-link" href="<?php print $link_target; ?>" style="color:#<?php print $link_color; ?>;"><?php print $link_title; ?></a></p>
+    </div>
+  </div>
+</div>
---
+++ modules/custom/nesi_slideshow/templates/nesi-slideshow-wrapper.tpl.php
@@ -0,0 +1,9 @@
+<div class="hero hero-full">
+  <div id="home-hero" class="carousel slide">
+  <div class="carousel-inner">
+  <?php print $content; ?>
+  </div>
+  <a data-slide="prev" href="#home-hero" class="carousel-control left">‹</a>
+  <a data-slide="next" href="#home-hero" class="carousel-control right">›</a>
+  </div>
+</div>
