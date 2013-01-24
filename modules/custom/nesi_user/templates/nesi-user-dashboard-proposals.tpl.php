1483b1dde187c79f595d2e51d77dee60506601cc
Nathaniel Dobbin <ndobbin@gmail.com>
2013-01-24 02:20:27 +0000

    Update styles.

--- modules/custom/nesi_user/templates/nesi-user-dashboard-proposals.tpl.php
+++ modules/custom/nesi_user/templates/nesi-user-dashboard-proposals.tpl.php
@@ -1,11 +1,9 @@
-<article class="project-outline">
+<article class="proposal-outline">
   <header>
-    <h1 class="project-title"><a href="<?php print $proposal_link; ?>"><?php print $proposal_title; ?></a></h1>
+    <h1 class="proposal-title"><a href="<?php print $proposal_link; ?>"><?php print $proposal_title; ?></a></h1>
   </header>
-  <div class="project-status">
+  <div class="proposal-status">
     <small>Status</small>
     <span class="label label-info"><?php print $proposal_state; ?></span>
-    <!--<small>Progress</small>
-    <div style="width: 20%;" class="bar"></div>-->
   </div>
 </article>
