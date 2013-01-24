47160389d37251de4386a8f2e7c0024fa6fff8da
Nathaniel Dobbin <ndobbin@gmail.com>
2013-01-24 01:29:53 +0000

    Clean up user dashboard to use templates.

---
+++ modules/custom/nesi_user/templates/nesi-user-dashboard-create-proposal.tpl.php
@@ -0,0 +1,31 @@
+<article role="main">
+  <div class="module centered-module bordered-module">
+    <div>
+      <article id="create-proposal">
+        <h1>Create a proposal</h1>
+        <div class="content">
+          <div class="module-actions">
+            <a class="btn btn-large" href="/node/add/proposal-development-class">Development Proposal</a>
+            <a class="btn btn-large" href="/node/add/proposal-research-class">Research Proposal</a>
+            <a class="learn-more" href="/allocations">Learn more about the different proposals</a>
+          </div>
+        </div>
+      </article>
+    </div>
+  </div>
+  <div>
+    <section>
+      <h2>Apply for access to HPC facilities</h2>
+        <article>
+          <h3>Development Proposal</h3>
+          <p>Easy to request with fast review cycles, open to receiving proposals on an ongoing on demand basis. PIs may request a single computational resource or a combination of resources. 
+          <a href="/allocations#proposal-development">Learn more</a></p>
+        </article>
+        <article>
+          <h3>Research Proposal</h3>
+          <p>Research allocations often follow successful completion of Proposal Development allocations. A PI must either successfully complete a Proposal Development allocation or be able to demonstrate prior relevant experience and success in running their codes on HPC facilities prior to receiving a Research allocation. Research allocations can be proposed to any public research funding body and for any of our HPC facilities.
+          <a href="/allocations#research">Learn more</a></p>
+        </article>
+    </section>
+  </div>
+</article>
---
+++ modules/custom/nesi_user/templates/nesi-user-dashboard-proposals.tpl.php
@@ -0,0 +1,32 @@
+<article class="project-outline">
+  <header>
+    <h1 class="project-title"><a href="<?php print $proposal_link; ?>"><?php print $proposal_title; ?></a></h1>
+  </header>
+  <div class="stats-pane">
+    <!--<div class="project-allocation-summary">
+      <h3 class="allocation-title">Allocation Summary</h3>
+      <p class="date">Allocation begins: 12/10/2012</p>
+      <p class="date">Allocation ends: 12/11/2012</p>
+    </div>-->
+    <div class="allocation-stats">
+      <!--<h2 class="project-platform-title">Platform: POWER 7</h2>-->
+      <div class="project-status">
+        <small>Status</small>
+        <span class="label label-info"><?php print $proposal_state; ?></span>
+        <!--<small>Progress</small>
+        <div style="width: 20%;" class="bar"></div>-->
+      </div>
+      <!--<ul class="stats-tabs">
+        <li>12 <span>CPUs</span></li>
+        <li>7680 <span>CPU hours</span></li>
+        <li>80 <span>Wall clock hours</span></li>
+      </ul>-->
+    </div>
+  </div>
+</article>
+<h2>Projects in progress</h2>
+<article class="project-outline">
+  <div class="alert alert-info">
+    It doesn\'t look like you have any live projects yet. Projects will appear here once an allocation is granted.
+  </div>
+</article>
