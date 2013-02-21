ad334d5763a85281fa30cc5549748001205ee9ca
katie <katie@webscope.co.nz>
2013-02-22 11:50:35 +1300

    Theme homepage slideshow

--- modules/custom/nesi_slideshow/templates/nesi-slideshow-item.tpl.php
+++ modules/custom/nesi_slideshow/templates/nesi-slideshow-item.tpl.php
@@ -4,9 +4,11 @@
     <div class="slide-content">
       <h2 class="slide-title">
         <span class="line1" style="color:#FFFFFF; background-color:#<?php print $primary_bg_color; ?>;"><?php print $primary_title; ?></span>
-        <span class="line2" style="color:#FFFFFF; background-color:#<?php print $secondary_bg_color; ?>;"><?php print $secondary_title; ?></span>
       </h2>
-      <p><a class="slide-link" href="<?php print $link_target; ?>" style="color:#<?php print $link_color; ?>;"><?php print $link_title; ?></a></p>
+      <h3>
+        <span class="line2" style="color:#FFFFFF; background-color:#<?php print $secondary_bg_color; ?>;"><?php print $secondary_title; ?></span>
+        <span class="line2" style="color:#FFFFFF; background-color:#<?php print $secondary_bg_color; ?>;"><?php print $secondary_title_line_2; ?></span>
+      </h3>
     </div>
   </div>
 </div>
