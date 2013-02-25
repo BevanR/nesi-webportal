ae9de247384c48b54c8e213d06c5079a3fddacf6
katie <katie@webscope.co.nz>
2013-02-25 16:59:01 +1300

    Start theming homepage stats

--- modules/custom/nesi_stats/nesi_stats_block.tpl.php
+++ modules/custom/nesi_stats/nesi_stats_block.tpl.php
@@ -1,16 +1,29 @@
 <?php
+  # There is a wrapper template to this template in the theme
+  # folder named block--nesi-stats--front-stats.tpl.php
+  #
   # Template for stats block that appears on the front page
   $data = $variables['data'];
 ?>
-<div id="stats_cpu_cores">
-  <?php print $data['stats_cpu_cores'] ?>
-</div>
-<div id="stats_memory">
-  <?php print $data['stats_memory'] ?>
-</div>
-<div id="stats_peak_speeds">
-  <?php print $data['stats_peak_speeds'] ?>
-</div>
-<div id="stats_disk_storage">
-  <?php print $data['stats_disk_storage'] ?>
+<div class="row-fluid">
+  <div id="stats_cpu_cores" class="span4">
+    <span class="stats-header">CPU Cores</span>
+    <div class="stats-stat"><?php print $data['stats_cpu_cores'] ?></div>
+    <span class="stats-description">Distributed across facilities</span>
+  </div>
+  <div id="stats_memory" class="span4">
+    <span class="stats-header">Memory</span>
+    <div class="stats-stat"><?php print $data['stats_memory'] ?></div>
+    <span class="stats-description">GB of RAM</span>
+  </div>
+  <div id="stats_peak_speeds" class="span4">
+    <span class="stats-header">Peak Speeds</span>
+    <div class="stats-stat"><?php print $data['stats_peak_speeds'] ?></div>
+    <span class="stats-description">&nbsp;</span>
+  </div>
+  <div id="stats_disk_storage" class="span4">
+    <span class="stats-header">Disk Storage</span>
+    <div class="stats-stat"><?php print $data['stats_disk_storage'] ?></div>
+    <span class="stats-description">Distributed across facilities</span>
+  </div>
 </div>
