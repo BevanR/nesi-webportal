667dba798c4ed533ebf6581bbc7e95c52d6b9788
katie <katie@webscope.co.nz>
2013-02-25 16:24:13 +1300

    Theming first page of form

--- themes/bootstrap/nesi_bootstrap/assets/js/custom.js
+++ themes/bootstrap/nesi_bootstrap/assets/js/custom.js
@@ -1,5 +1,32 @@
-$(document).ready(function() {
-	$('[rel="tooltip"]').tooltip();
-	$('#home-hero').carousel();
-	$('#home-hero .slide-title').lettering('lines').children('words');
-});
+( function($) {
+
+	$(document).ready(function() {
+
+  	/*
+    $('[rel="tooltip"]').tooltip();
+  	$('#home-hero').carousel();
+  	$('#home-hero .slide-title').lettering('lines').children('words');
+  	*/
+  
+  	/* Forms */
+  	$('#nesi-mstep-proposal-step-1 .form-text').each(function() {
+    	$('#nesi-mstep-proposal-step-1 .form-type-textfield label').hide();
+    	$('.form-required').remove();
+    	var id = $(this).attr('id');
+    	var label = $("label[for=" + id +"]").text();
+    	$(this).val(label);
+    	$(this).focus(function() {
+      	if ( $(this).val() == label ) {
+        	$(this).val('');
+      	}
+    	});
+    	$(this).blur(function() {
+      	if ( $(this).val() == '' ) {
+        	$(this).val(label);
+      	}
+    	});
+  	});
+
+	});
+
+} ) ( jQuery );
