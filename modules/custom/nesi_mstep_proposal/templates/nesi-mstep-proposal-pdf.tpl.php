40d8cd9da8eb069b32264766a25f28bd1c43238b
Sam <sam.blackmore@gmail.com>
2013-03-22 11:05:57 +1300

    data for proposals being returned properly

--- modules/custom/nesi_mstep_proposal/templates/nesi-mstep-proposal-pdf.tpl.php
+++ modules/custom/nesi_mstep_proposal/templates/nesi-mstep-proposal-pdf.tpl.php
@@ -1,6 +1,17 @@
 <?php
 
-print_r($variables);
+$data = $variables['data'];
 
 ?>
 <h1>Coming from the Proposal Pdf template</h1>
+
+<?php foreach($data as $field) :?>
+
+<h3><?php print $field['field_label']  ?></h3>
+<p><?php print $field['field_val']  ?></p>
+
+
+
+<?php
+  endforeach;
+?>
