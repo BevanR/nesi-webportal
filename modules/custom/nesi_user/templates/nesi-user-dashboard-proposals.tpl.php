f37a62f2a220ebccf7d660f08794915761d82e0f
katie <katie@webscope.co.nz>
2013-03-22 13:59:35 +1300

    Theming Dashboard

--- modules/custom/nesi_user/templates/nesi-user-dashboard-proposals.tpl.php
+++ modules/custom/nesi_user/templates/nesi-user-dashboard-proposals.tpl.php
@@ -1,6 +1,19 @@
+<?php
+switch ( $proposal_type ) {
+  case 'proposal_research_class' :
+    $type = 'Research Proposal';
+    break;
+  case 'proposal_development_class' :
+    $type = 'Development Proposal';
+    break;
+  case 'proposal_collaborator_class' :
+    $type = 'Collaborator Proposal';
+    break;
+}
+?>
 <article class="proposal-outline">
   <header>
-    <h1 class="proposal-title"><a href="<?php print $proposal_link; ?>"><?php print $proposal_title; ?></a></h1>
+    <h1 class="proposal-title"><a href="<?php print $proposal_link; ?>"><strong><?php print $type; ?> - <?php print $proposal_platform; ?></strong><?php print $proposal_title; ?></a></h1>
   </header>
   <div class="proposal-status">
     <small>Status</small>
