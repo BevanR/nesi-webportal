14a4e0886d44a69da8120f6986e740d9a6e6ed9e
katie <katie@webscope.co.nz>
2013-03-22 16:01:38 +1300

    Theming dashboard

--- modules/custom/nesi_user/templates/nesi-user-dashboard-proposals.tpl.php
+++ modules/custom/nesi_user/templates/nesi-user-dashboard-proposals.tpl.php
@@ -13,11 +13,9 @@ switch ( $proposal_type ) {
 ?>
 <article class="proposal-outline">
   <header>
-    <h1 class="proposal-title"><a href="<?php print $proposal_link; ?>"><strong><?php print $type; ?> - <?php print $proposal_platform; ?></strong><?php print $proposal_title; ?></a></h1>
+    <h1 class="proposal-title"><a href="<?php print $proposal_link; ?>"><strong><?php print $type; ?> - <?php print $proposal_platform; ?><?php if ($proposal_state) : ?> - <span><?php print $proposal_state; ?></span><?php endif; ?></strong><?php print $proposal_title; ?></a></h1>
   </header>
   <div class="proposal-status">
-    <small>Status</small>
-    <span class="label label-info"><?php print $proposal_state; ?></span>
     <?php print $proposal_operations; ?>
   </div>
 </article>
