f3f33eb7e2733a9e23aab0f7b86fbc39248d7324
Sam <sam.blackmore@gmail.com>
2013-03-25 11:23:38 +1300

    proposals outputting to a pdf template - template suggestions not working

--- modules/custom/nesi_mstep_proposal/templates/nesi-mstep-proposal-pdf.tpl.php
+++ modules/custom/nesi_mstep_proposal/templates/nesi-mstep-proposal-pdf.tpl.php
@@ -1,17 +1,15 @@
 <?php
 
 $data = $variables['data'];
-
 ?>
 <h1>Coming from the Proposal Pdf template</h1>
 
-<?php foreach($data as $field) :?>
+<?php foreach($data as $field => $val) :?>
 
-<h3><?php print $field['field_label']  ?></h3>
-<p><?php print $field['field_val']  ?></p>
-
-
+<h3><?php print $val['field_label']  ?></h3>
+<p><?php print $val['field_val']  ?></p>
 
 <?php
   endforeach;
+
 ?>
