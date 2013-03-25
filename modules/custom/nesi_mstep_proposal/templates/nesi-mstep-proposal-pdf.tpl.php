5a7fb318858343819f01b601fe3b34f56dab6b1d
Sam <sam.blackmore@gmail.com>
2013-03-26 12:18:41 +1300

    added a map to the template

--- modules/custom/nesi_mstep_proposal/templates/nesi-mstep-proposal-pdf.tpl.php
+++ modules/custom/nesi_mstep_proposal/templates/nesi-mstep-proposal-pdf.tpl.php
@@ -6,24 +6,28 @@
  */
 $data = $variables['data'];
 //print_r($data);
+$development_class_map = array(
+  'software_installation' => 'Software Installation',
+  'software_porting'      => 'Software Porting',
+  'software_optimisation' => 'Software Optimisation',
+  'scaling_performance'   => 'Scaling Performance',
+);
 ?>
 <?php foreach($data as $field => $val) :?>
 
-  <?php// print_r($field)  ?>
   <h3><?php print $val['field_label']  ?></h3>
 
   <?php
     if(is_array($val['field_val'])):
-      // Need to know how this is going to be formated
-      // speak with Sam 
-      print_r($val['field_val']);
+      foreach($val['field_val'] as $key) {
+        print $development_class_map[$key];
+      }
     else:
   ?>
   <p><?php print $val['field_val']  ?></p>
   <?php  
     endif;
   ?>
-
   
 <?php
   endforeach;
