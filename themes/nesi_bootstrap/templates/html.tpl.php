5973043a4a065b3ebab2303d9b637b7be458e8bf
Bevan Rudge <bevan@drupal.geek.nz>
2013-04-12 15:35:16 +1200

    Remove Favicon.ico since there is no favicon file there.

--- themes/nesi_bootstrap/templates/html.tpl.php
+++ themes/nesi_bootstrap/templates/html.tpl.php
@@ -9,7 +9,6 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="<?php print $directory; ?>/favicon.ico" type="image/x-icon" />
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
