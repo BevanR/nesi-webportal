46ead37e150dbcd8de84b63e33a0a1fd1966ac67
katie <katie@webscope.co.nz>
2013-04-17 14:42:56 +1200

    Fixes BugHerd #111 - Remove ID from submissions proposal view

--- modules/custom/nesi_user/templates/nesi-user-dashboard-proposals.tpl.php
+++ modules/custom/nesi_user/templates/nesi-user-dashboard-proposals.tpl.php
@@ -14,18 +14,22 @@ switch ( $proposal_type ) {
 <div class="proposal-outline-wrapper">
   <article class="proposal-outline">
     <header>
-      <h1 class="proposal-title"><a href="<?php print $proposal_link; ?>"><strong><?php print $type; ?> - <?php print strtoupper($proposal_platform); ?></strong><?php if ($proposal_state) : ?> - <span><?php print $proposal_state; ?></span><?php endif; ?></a><span class="proposal-operations"><?php print $proposal_operations; ?></span><br /><?php print $proposal_title; ?></h1>
+      <h1 class="proposal-title"><a href="<?php print $proposal_link; ?>"><strong><?php print $type; ?><?php if ( $proposal_platform != 'unspecified' ) : ?> - <?php print strtoupper($proposal_platform); ?><?php endif; ?></strong><?php if ($proposal_state) : ?> - <span><?php print $proposal_state; ?></span><?php endif; ?></a><span class="proposal-operations"><?php print $proposal_operations; ?></span><br /><?php print $proposal_title; ?></h1>
     </header>
     <div class="proposal-meta">
+      <!--
       <div>
         <?php print $proposal_id; ?>
       </div>
+      -->
       <div>
         <?php print $proposal_start_date; ?>
       </div>
+      <!--
       <div>
         TODO
       </div>
+      -->
       <div>
         <?php print $proposal_cpu_hours; ?>
       </div>
